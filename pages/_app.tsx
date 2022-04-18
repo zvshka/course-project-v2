1c44628ecb99128dd397668a5c05f8ccda5e966b
zvshka <pushpurs@yandex.ru>
2022-04-18 18:56:15 +0300

    More styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,36 @@ import {ReactQueryDevtools} from 'react-query/devtools'
 import {MantineProvider} from '@mantine/core';
 import {NotificationsProvider} from "@mantine/notifications";
 import {ModalsProvider} from "@mantine/modals";
+import Head from "next/head";
+import {GetServerSidePropsContext} from "next";
 const queryClient = new QueryClient()
 
 function MyApp({Component, pageProps}) {
-    return <QueryClientProvider client={queryClient}>
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            emotionOptions={{key: 'mantin', prepend: false}}
-            theme={{
-                colorScheme: 'light',
-            }}
-        >
-            <NotificationsProvider>
-                <ModalsProvider>
-                    <Component {...pageProps} />
-                    <ReactQueryDevtools initialIsOpen={false}/>
-                </ModalsProvider>
-            </NotificationsProvider>
-        </MantineProvider>
-    </QueryClientProvider>
+    return <>
+        <Head>
+            <title>Fantastic Waffle</title>
+            <meta charSet="utf-8" />
+            <link rel="shortcut icon" href="/food-waffles.svg" />
+        </Head>
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                emotionOptions={{key: 'mantin', prepend: false}}
+                theme={{
+                    colorScheme: 'light',
+                }}
+            >
+                <NotificationsProvider>
+                    <ModalsProvider>
+                        <Component {...pageProps} />
+                        <ReactQueryDevtools initialIsOpen={false}/>
+                    </ModalsProvider>
+                </NotificationsProvider>
+            </MantineProvider>
+        </QueryClientProvider>
+    </>
 }
+MyApp.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({});
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,13 @@ const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
     static getInitialProps = getInitialProps;
-    //
+
     // render(): JSX.Element {
     //     return <Html>
-    //         <Head>
-    //             <title>Fantastic Waffle</title>
-    //             {/*<meta name="viewport" content="initial-scale=1.0, width=device-width" />*/}
-    //         </Head>
+    //
     //         <body>
-    //         <Main/>
-    //         <NextScript/>
+    //             <Main/>
+    //             <NextScript/>
     //         </body>
     //     </Html>
     // }
